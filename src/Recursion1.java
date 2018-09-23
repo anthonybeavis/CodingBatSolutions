6a342f90a89be5a8a1252d9cd3dd65d230e7fa17
Anthony Beavis <anth.beavis@gmail.com>
2018-09-23 17:03:24 +0100

    First commit

---
+++ src/Recursion1.java
@@ -0,0 +1,16 @@
+public class Recursion1 {
+	
+	public static int factorial(int n) {
+		  if(n==0){ 
+		    return 1;
+		  }
+		  return n*(factorial(n-1));
+		}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
